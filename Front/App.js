11a47a5e9605f356ff0ab3b756a07df410679b5b
ASmithie <alecsmith_@outlook.com>
2021-01-28 02:03:24 +0000

    I fucked with the navbar cause why not

--- Front/App.js
+++ Front/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, StatusBar } from "react-native";
+import { StyleSheet, SafeAreaView, StatusBar, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Ionicons from "react-native-vector-icons/Ionicons";
@@ -11,57 +11,83 @@ import Channels from "./Components/channels";
 import Upload from "./Components/UploadScreen";
 import Map from "./Components/map";
 
-const bButton = createBottomTabNavigator(); // Create the bottom tab bar
+const bButton = createMaterialBottomTabNavigator(); // Create the bottom tab bar
 
 export default function App() {
     // Main app function
+
     return (
         <SafeAreaView style={styles.container}>
             <NavigationContainer>
                 <bButton.Navigator // Sets things about the bottom buttons
-                    screenOptions={({ route }) => ({
-                        tabBarIcon: ({ focused, color, size }) => {
-                            let iconName;
-
-                            if (route.name === "Home") {
-                                // Such as the icons
-                                iconName = focused ? "home" : "home-outline";
-                            } else if (route.name === "Browse") {
-                                iconName = focused ? "list" : "list-outline";
-                            } else if (route.name === "Channels") {
-                                iconName = focused
-                                    ? "albums"
-                                    : "albums-outline";
-                            } else if (route.name === "Upload") {
-                                iconName = focused
-                                    ? "add-circle"
-                                    : "add-circle-outline";
-                            } else if (route.name === "Map") {
-                                iconName = focused ? "map" : "map-outline";
-                            }
-
-                            return (
-                                <Ionicons
-                                    name={iconName}
-                                    size={size}
-                                    color={color}
-                                />
-                            );
-                        },
-                    })}
-                    tabBarOptions={{
-                        activeTintColor: "blue", // Colour of the bar
-                        inactiveTintColor: "gray",
-                    }}
+                    initialRouteName='Browse'
                 >
-                    <bButton.Screen name='Home' component={Home} />
-                    <bButton.Screen name='Browse' component={Browse} />
-                    <bButton.Screen name='Channels' component={Channels} />
-                    <bButton.Screen name='Upload' component={Upload} />
-                    <bButton.Screen name='Map' component={Map} />
+                    <bButton.Screen
+                        name='Home'
+                        component={Home}
+                        options={{
+                            tabBarColor: "purple",
+                            tabBarIcon: ({ color }) => (
+                                <Ionicons
+                                    name='person-circle'
+                                    color={color}
+                                    size={22}
+                                />
+                            ),
+                        }}
+                    />
+                    <bButton.Screen
+                        name='Channels'
+                        component={Channels}
+                        options={{
+                            tabBarColor: "tomato",
+                            tabBarIcon: ({ color }) => (
+                                <Ionicons
+                                    name='albums'
+                                    color={color}
+                                    size={22}
+                                />
+                            ),
+                        }}
+                    />
+                    <bButton.Screen
+                        name='Browse'
+                        component={Browse}
+                        options={{
+                            tabBarColor: "blue",
+                            tabBarIcon: ({ color }) => (
+                                <Ionicons name='home' color={color} size={22} />
+                            ),
+                        }}
+                    />
+                    <bButton.Screen
+                        name='Upload'
+                        component={Upload}
+                        options={{
+                            tabBarColor: "brown",
+                            tabBarIcon: ({ color }) => (
+                                <Ionicons
+                                    name='add-circle'
+                                    color={color}
+                                    size={22}
+                                />
+                            ),
+                        }}
+                    />
+                    <bButton.Screen
+                        name='Map'
+                        component={Map}
+                        options={{
+                            tabBarColor: "orange",
+                            tabBarIcon: ({ color }) => (
+                                <Ionicons name='map' color={color} size={22} />
+                            ),
+                        }}
+                    />
                 </bButton.Navigator>
             </NavigationContainer>
-        </SafeAreaView> // Creates the buttons and sets which page function they call
+        </SafeAreaView>
+        // Creates the buttons and sets which page function they call
     ); // options={{tabBarBadge: 0} can be used to set notification nubers
 }
 
