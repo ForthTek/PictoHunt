c1b498b4fafd546776f3b54bcb2cc1b7851398d5
Eoghann Gibson <48491515+EoghannGibson@users.noreply.github.com>
2021-01-31 09:04:01 +0000

    Add files via upload

---
+++ Front/Components/login.js
@@ -0,0 +1,198 @@
+import React from "react";
+import { Text, View, SafeAreaView, StyleSheet, TextInput, Button} from "react-native";
+import Image from "react-native-scalable-image";
+import Score from "./score";
+export default function Home() {
+  
+
+   
+
+    var Pictohunt = "Pictohunt"
+    var byforthtek = "By ForthTek"
+    var logint = "Login"
+    var email = "Email:"
+    var password = "Password:"
+    var [value, onChangeText] = React.useState("");
+    var [value2, onChangeText] = React.useState("");
+    var guest = "Proceed as Guest";
+    var forgot = "Forgot Password";
+    var create = "Create an Account";
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={{ flexDirection: "row", margin: "0%", marginLeft: "30%"}}>
+                <Image
+                    source={require("../assets/ForthTek.png")}
+                    width={160}
+                />
+
+                
+            </View>
+            <View
+                style={{
+                    flexDirection: "row",
+                    paddingBottom: "5%",
+                    justifyContent: "space-between",
+                }}
+            >
+            </View>
+
+             <Text style={styles.Pictohunt}>{Pictohunt}</Text>
+             <Text style={styles.byforthtek}>{byforthtek}</Text>
+             <Text style={styles.logint}>{logint}</Text>
+             
+            <View
+                style={{
+                    flexDirection: "row",
+                    paddingBottom: "5%",
+                    justifyContent: "space-between",
+                }}
+            >
+                
+                    <Text style={styles.email}>{email}</Text>
+                    
+                    <TextInput 
+                    style={styles.input2}
+                    onChangeText={(text) => onChangeText(text)}
+                    value={value}
+                     />
+
+                     </View>
+                     
+                    
+            <View
+                style={{
+                    flexDirection: "row",
+                    paddingBottom: "5%",
+                    justifyContent: "space-between",
+                }}
+            >
+                
+                    <Text style={styles.password}>{password}</Text>
+                    
+                    <TextInput 
+                    style={styles.input2}
+                    onChangeText={(text) => onChangeText(text)}
+                    value={value}
+                     />
+
+                     </View>
+
+
+            
+                    <Button
+                    style={styles.button}
+
+                    onPress={() => this._handlePress()}
+                    title="Login"
+                    >
+                    </Button>
+
+                    <Text style={styles.guest}>{guest}</Text>
+
+                    <View
+                style={{
+                    flexDirection: "row",
+                    paddingBottom: "5%",
+                    justifyContent: "space-between",
+                }}
+            >
+                
+                    <Text style={styles.forgot}>{forgot}</Text>
+                    <Text style={styles.create}>{create}</Text>
+
+                </View>
+
+
+            
+        </SafeAreaView>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        padding: "1%",
+    },
+    Pictohunt: {
+        fontSize: 35,
+        paddingBottom: "2%",
+        paddingLeft:"30%",
+
+    },
+    byforthtek: {
+        fontSize: 18,
+        paddingBottom: "5%",
+        paddingLeft:"36.5%",
+    },
+
+    logint: {
+        fontSize: 30,
+        paddingBottom: "5%",
+        paddingLeft:"40%",
+        paddingTop:"10%",
+    },
+    input: {
+        height:45,
+        borderColor: "gray",
+        borderWidth: 1,
+        marginBottom: "0%",
+        width: "80%",
+        fontSize: 20,
+        color: "gray",
+    },
+    input2: {
+        height:45,
+        borderColor: "gray",
+        borderWidth: 1,
+        marginBottom: "0%",
+        width: "70%",
+        fontSize: 20,
+        color: "gray",
+    },
+    email: {
+        fontSize: 20,
+        paddingBottom: "10%",
+        paddingRight:"16.6%",
+        paddingTop:"0%",
+
+    },
+    password: {
+        fontSize: 20,
+        paddingBottom: "10%",
+        paddingLeft:"0%",
+        paddingTop:"0%",
+
+    },
+    button: {
+        fontSize: 18,
+        paddingTop: "5%",
+        color:"red",
+
+    },
+    guest: {
+        fontSize: 15,
+        color: "blue",
+        paddingBottom: "10%",
+        paddingLeft:"35%",
+        paddingTop:"15%",
+        textDecorationLine:"underline",
+    },
+    forgot: {
+        fontSize: 15,
+        color: "blue",
+        paddingBottom: "10%",
+        paddingLeft:"12%",
+        paddingTop:"5%",
+        textDecorationLine:"underline",
+    },
+    create: {
+        fontSize: 15,
+        color: "blue",
+        paddingBottom: "10%",
+        paddingRight:"12%",
+        paddingTop:"5%",
+        textDecorationLine:"underline",
+    },
+});
+
