9b0eac70062af18c086c8427bee74588c1ec0892
Solomon Baarda <50365603+SolomonBaarda@users.noreply.github.com>
2021-02-23 10:51:11 +0000

    server changes

--- Back/server.js
+++ Back/server.js
@@ -1,22 +1,100 @@
-const express = require("express");
+// Followed tutorial from:
+// https://github.com/satansdeer/firebase-server-auth
+// https://www.youtube.com/watch?v=kX8by4eCyG4
+
+const cookieParser = require("cookie-parser");
+const csrf = require("csurf");
 const bodyParser = require("body-parser");
+const express = require("express");
+const admin = require("firebase-admin");
 
-const app = express();
-const port = process.env.PORT || 5000;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const auth = require("./Components/firebase-auth.js");
-
-var admin = require("firebase-admin");
-
-var serviceAccount = require("./serviceAccountKey.json");
+const auth = require("./serviceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(auth),
+  //databaseURL: "https://server-auth-41acc.firebaseio.com",
 });
 
+const csrfMiddleware = csrf({ cookie: true });
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.engine("html", require("ejs").renderFile);
+app.use(express.static("static"));
+
+/** DO WE NEED THIS? */
+//app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(csrfMiddleware);
+
+app.all("*", (req, res, next) => {
+  res.cookie("XSRF-TOKEN", req.csrfToken());
+  next();
+});
+
+app.get("/login", function (req, res) {
+  res.render("login.html");
+});
+
+app.get("/signup", function (req, res) {
+  res.render("signup.html");
+});
+
+app.get("/profile", function (req, res) {
+  const sessionCookie = req.cookies.session || "";
+
+  // Check that the user has the required session cookie
+  admin
+    .auth()
+    .verifySessionCookie(sessionCookie, true /** checkRevoked */)
+    .then(() => {
+      res.render("profile.html");
+    })
+    // If not, force them to login
+    .catch((error) => {
+      res.redirect("/login");
+    });
+});
+
+app.get("/", function (req, res) {
+  res.redirect("/browse");
+});
+
+app.get("/browse", function (req, res) {
+  // Home screen
+});
+
+app.post("/sessionLogin", (req, res) => {
+  const idToken = req.body.idToken.toString();
+
+  // Cookie expires in 5 days
+  const expiresIn = 60 * 60 * 24 * 5 * 1000;
+
+  admin
+    .auth()
+    .createSessionCookie(idToken, { expiresIn })
+    // Create the session cookie for the user
+    .then(
+      (sessionCookie) => {
+        const options = { maxAge: expiresIn, httpOnly: true };
+        res.cookie("session", sessionCookie, options);
+        res.end(JSON.stringify({ status: "success" }));
+      },
+      (error) => {
+        res.status(401).send("UNAUTHORIZED REQUEST!");
+      }
+    );
+});
+
+app.get("/sessionLogout", (req, res) => {
+  res.clearCookie("session");
+  res.redirect("/login");
+});
+
+/** TODO ??? */
 app.get("/api/getUser", (req, res) => {
   (async () => {
     let value = await api.getUser(USERNAME);
@@ -25,10 +103,6 @@ app.get("/api/getUser", (req, res) => {
   })();
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
-
-
-
-
-
-
+app.listen(PORT, () => {
+  app.listen(port, () => console.log(`Listening on port ${port}`));
+});
