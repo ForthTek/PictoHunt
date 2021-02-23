cac8c43d59e6f4b1aa840052e16460aea585a3b9
ASmithie <alecsmith_@outlook.com>
2021-02-23 14:09:56 +0000

    Login screen up. press login to be logged in

--- Front/App.js
+++ Front/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { StyleSheet, SafeAreaView, StatusBar, View } from "react-native"
 import { NavigationContainer } from "@react-navigation/native"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
@@ -10,85 +10,106 @@ import Browse from "./Components/browse"
 import Channels from "./Components/channels"
 import Upload from "./Components/UploadScreen"
 import Map from "./Components/map"
+import Login from "./Components/login"
 
 const bButton = createMaterialBottomTabNavigator() // Create the bottom tab bar
 
 export default function App() {
     // Main app function
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <NavigationContainer>
-                <bButton.Navigator // Sets things about the bottom buttons
-                    initialRouteName='Browse'
-                >
-                    <bButton.Screen
-                        name='Home'
-                        component={Home}
-                        options={{
-                            tabBarColor: "black",
-                            tabBarIcon: ({ color }) => (
-                                <Ionicons
-                                    name='person-circle'
-                                    color={color}
-                                    size={22}
-                                />
-                            ),
-                        }}
-                    />
-                    <bButton.Screen
-                        name='Channels'
-                        component={Channels}
-                        options={{
-                            tabBarColor: "tomato",
-                            tabBarIcon: ({ color }) => (
-                                <Ionicons
-                                    name='albums'
-                                    color={color}
-                                    size={22}
-                                />
-                            ),
-                        }}
-                    />
-                    <bButton.Screen
-                        name='Browse'
-                        component={Browse}
-                        options={{
-                            tabBarColor: "blue",
-                            tabBarIcon: ({ color }) => (
-                                <Ionicons name='home' color={color} size={22} />
-                            ),
-                        }}
-                    />
-                    <bButton.Screen
-                        name='Upload'
-                        component={Upload}
-                        options={{
-                            tabBarColor: "brown",
-                            tabBarIcon: ({ color }) => (
-                                <Ionicons
-                                    name='add-circle'
-                                    color={color}
-                                    size={22}
-                                />
-                            ),
-                        }}
-                    />
-                    <bButton.Screen
-                        name='Map'
-                        component={Map}
-                        options={{
-                            tabBarColor: "orange",
-                            tabBarIcon: ({ color }) => (
-                                <Ionicons name='map' color={color} size={22} />
-                            ),
-                        }}
-                    />
-                </bButton.Navigator>
-            </NavigationContainer>
-        </SafeAreaView>
-        // Creates the buttons and sets which page function they call
-    ) // options={{tabBarBadge: 0} can be used to set notification nubers
+    if (isLoggedIn) {
+        return (
+            <SafeAreaView style={styles.container}>
+                <NavigationContainer>
+                    <bButton.Navigator // Sets things about the bottom buttons
+                        initialRouteName='Browse'
+                    >
+                        <bButton.Screen
+                            name='Home'
+                            component={Home}
+                            options={{
+                                tabBarColor: "black",
+                                tabBarIcon: ({ color }) => (
+                                    <Ionicons
+                                        name='person-circle'
+                                        color={color}
+                                        size={22}
+                                    />
+                                ),
+                            }}
+                        />
+                        <bButton.Screen
+                            name='Channels'
+                            component={Channels}
+                            options={{
+                                tabBarColor: "tomato",
+                                tabBarIcon: ({ color }) => (
+                                    <Ionicons
+                                        name='albums'
+                                        color={color}
+                                        size={22}
+                                    />
+                                ),
+                            }}
+                        />
+                        <bButton.Screen
+                            name='Browse'
+                            component={Browse}
+                            options={{
+                                tabBarColor: "blue",
+                                tabBarIcon: ({ color }) => (
+                                    <Ionicons
+                                        name='home'
+                                        color={color}
+                                        size={22}
+                                    />
+                                ),
+                            }}
+                        />
+                        <bButton.Screen
+                            name='Upload'
+                            component={Upload}
+                            options={{
+                                tabBarColor: "brown",
+                                tabBarIcon: ({ color }) => (
+                                    <Ionicons
+                                        name='add-circle'
+                                        color={color}
+                                        size={22}
+                                    />
+                                ),
+                            }}
+                        />
+                        <bButton.Screen
+                            name='Map'
+                            component={Map}
+                            options={{
+                                tabBarColor: "orange",
+                                tabBarIcon: ({ color }) => (
+                                    <Ionicons
+                                        name='map'
+                                        color={color}
+                                        size={22}
+                                    />
+                                ),
+                            }}
+                        />
+                    </bButton.Navigator>
+                </NavigationContainer>
+            </SafeAreaView>
+            // Creates the buttons and sets which page function they call
+        ) // options={{tabBarBadge: 0} can be used to set notification nubers
+    } else {
+        return (
+            <Login
+                login={() => {
+                    setIsLoggedIn(true)
+                    console.log("login")
+                }}
+            />
+        )
+    }
 }
 
 const styles = StyleSheet.create({
--- Front/Components/login.js
+++ Front/Components/login.js
@@ -1,46 +1,65 @@
-import React from "react";
-import { Text, View, SafeAreaView, StyleSheet, TextInput, Button} from "react-native";
-import Image from "react-native-scalable-image";
-import Score from "./score";
-export default function Home() {
-  
+import React from "react"
+import {
+    Text,
+    View,
+    SafeAreaView,
+    StyleSheet,
+    TextInput,
+    Button,
+} from "react-native"
+import Image from "react-native-scalable-image"
 
-   
-
-    var Pictohunt = "Pictohunt"
-    var byforthtek = "By ForthTek"
-    var logint = "Login"
-    var email = "Email:"
-    var password = "Password:"
-    var [value, onChangeText] = React.useState("");
-    var [value2, onChangeText] = React.useState("");
-    var guest = "Proceed as Guest";
-    var forgot = "Forgot Password";
-    var create = "Create an Account";
+export default function Home(props) {
+    const Pictohunt = "Pictohunt"
+    const byforthtek = "By ForthTek"
+    const logint = "Login"
+    const email = "Email:"
+    const password = "Password:"
+    const [value, onChangeText] = React.useState("")
+    const [value2, onChangeText2] = React.useState("")
+    const guest = "Proceed as Guest"
+    const forgot = "Forgot Password"
+    const create = "Create an Account"
 
     return (
         <SafeAreaView style={styles.container}>
-            <View style={{ flexDirection: "row", margin: "0%", marginLeft: "30%"}}>
-                <Image
-                    source={require("../assets/ForthTek.png")}
-                    width={160}
+            <View
+                style={{
+                    flexDirection: "row",
+                    margin: "0%",
+                    marginLeft: "30%",
+                }}
+            >
+                <Image source={require("../assets/ForthTek.png")} width={160} />
+            </View>
+            <View
+                style={{
+                    flexDirection: "row",
+                    paddingBottom: "5%",
+                    justifyContent: "space-between",
+                }}
+            ></View>
+
+            <Text style={styles.Pictohunt}>{Pictohunt}</Text>
+            <Text style={styles.byforthtek}>{byforthtek}</Text>
+            <Text style={styles.logint}>{logint}</Text>
+
+            <View
+                style={{
+                    flexDirection: "row",
+                    paddingBottom: "5%",
+                    justifyContent: "space-between",
+                }}
+            >
+                <Text style={styles.email}>{email}</Text>
+
+                <TextInput
+                    style={styles.input2}
+                    onChangeText={(text) => onChangeText(text)}
+                    value={value}
                 />
-
-                
-            </View>
-            <View
-                style={{
-                    flexDirection: "row",
-                    paddingBottom: "5%",
-                    justifyContent: "space-between",
-                }}
-            >
             </View>
 
-             <Text style={styles.Pictohunt}>{Pictohunt}</Text>
-             <Text style={styles.byforthtek}>{byforthtek}</Text>
-             <Text style={styles.logint}>{logint}</Text>
-             
             <View
                 style={{
                     flexDirection: "row",
@@ -48,18 +67,23 @@ export default function Home() {
                     justifyContent: "space-between",
                 }}
             >
-                
-                    <Text style={styles.email}>{email}</Text>
-                    
-                    <TextInput 
-                    style={styles.input2}
-                    onChangeText={(text) => onChangeText(text)}
-                    value={value}
-                     />
+                <Text style={styles.password}>{password}</Text>
 
-                     </View>
-                     
-                    
+                <TextInput
+                    style={styles.input2}
+                    onChangeText={(text) => onChangeText2(text)}
+                    value={value2}
+                />
+            </View>
+
+            <Button
+                style={styles.button}
+                onPress={props.login}
+                title='Login'
+            ></Button>
+
+            <Text style={styles.guest}>{guest}</Text>
+
             <View
                 style={{
                     flexDirection: "row",
@@ -67,46 +91,11 @@ export default function Home() {
                     justifyContent: "space-between",
                 }}
             >
-                
-                    <Text style={styles.password}>{password}</Text>
-                    
-                    <TextInput 
-                    style={styles.input2}
-                    onChangeText={(text) => onChangeText(text)}
-                    value={value}
-                     />
-
-                     </View>
-
-
-            
-                    <Button
-                    style={styles.button}
-
-                    onPress={() => this._handlePress()}
-                    title="Login"
-                    >
-                    </Button>
-
-                    <Text style={styles.guest}>{guest}</Text>
-
-                    <View
-                style={{
-                    flexDirection: "row",
-                    paddingBottom: "5%",
-                    justifyContent: "space-between",
-                }}
-            >
-                
-                    <Text style={styles.forgot}>{forgot}</Text>
-                    <Text style={styles.create}>{create}</Text>
-
-                </View>
-
-
-            
+                <Text style={styles.forgot}>{forgot}</Text>
+                <Text style={styles.create}>{create}</Text>
+            </View>
         </SafeAreaView>
-    );
+    )
 }
 const styles = StyleSheet.create({
     container: {
@@ -117,23 +106,22 @@ const styles = StyleSheet.create({
     Pictohunt: {
         fontSize: 35,
         paddingBottom: "2%",
-        paddingLeft:"30%",
-
+        paddingLeft: "30%",
     },
     byforthtek: {
         fontSize: 18,
         paddingBottom: "5%",
-        paddingLeft:"36.5%",
+        paddingLeft: "36.5%",
     },
 
     logint: {
         fontSize: 30,
         paddingBottom: "5%",
-        paddingLeft:"40%",
-        paddingTop:"10%",
+        paddingLeft: "40%",
+        paddingTop: "10%",
     },
     input: {
-        height:45,
+        height: 45,
         borderColor: "gray",
         borderWidth: 1,
         marginBottom: "0%",
@@ -142,7 +130,7 @@ const styles = StyleSheet.create({
         color: "gray",
     },
     input2: {
-        height:45,
+        height: 45,
         borderColor: "gray",
         borderWidth: 1,
         marginBottom: "0%",
@@ -153,46 +141,42 @@ const styles = StyleSheet.create({
     email: {
         fontSize: 20,
         paddingBottom: "10%",
-        paddingRight:"16.6%",
-        paddingTop:"0%",
-
+        paddingRight: "16.6%",
+        paddingTop: "0%",
     },
     password: {
         fontSize: 20,
         paddingBottom: "10%",
-        paddingLeft:"0%",
-        paddingTop:"0%",
-
+        paddingLeft: "0%",
+        paddingTop: "0%",
     },
     button: {
         fontSize: 18,
         paddingTop: "5%",
-        color:"red",
-
+        color: "red",
     },
     guest: {
         fontSize: 15,
         color: "blue",
         paddingBottom: "10%",
-        paddingLeft:"35%",
-        paddingTop:"15%",
-        textDecorationLine:"underline",
+        paddingLeft: "35%",
+        paddingTop: "15%",
+        textDecorationLine: "underline",
     },
     forgot: {
         fontSize: 15,
         color: "blue",
         paddingBottom: "10%",
-        paddingLeft:"12%",
-        paddingTop:"5%",
-        textDecorationLine:"underline",
+        paddingLeft: "12%",
+        paddingTop: "5%",
+        textDecorationLine: "underline",
     },
     create: {
         fontSize: 15,
         color: "blue",
         paddingBottom: "10%",
-        paddingRight:"12%",
-        paddingTop:"5%",
-        textDecorationLine:"underline",
+        paddingRight: "12%",
+        paddingTop: "5%",
+        textDecorationLine: "underline",
     },
-});
-
+})
