25ec30b94c6cbda1e574e5795b4b683205cd35a6
Cameron <55835365+CJB15@users.noreply.github.com>
2021-02-27 16:40:45 +0000

    Now pulls markers from localhost:5000/map

--- Front/Components/map.js
+++ Front/Components/map.js
@@ -1,68 +1,56 @@
-import React from "react";
-import { Text, StyleSheet, SafeAreaView, View, Image , Alert} from "react-native";
+import React, {useState, Component} from "react";
+import { Text, StyleSheet, SafeAreaView, View, Image , Alert } from "react-native";
 import MapView, { Marker } from 'react-native-maps';
 import Geolocation from 'react-native-geolocation-service';
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
 import Geocoder from 'react-native-geocoding';
 
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+
 Geocoder.init('AIzaSyBcGAtTu4TlPOaWcObIqQisYnvEvlAqH1Y', {language : "en"});
 
 
 
-export default function Map() {
+export default class Map extends Component{
     // Map Page
-    var initialRegion={
-      latitude: 5.464497,
-      longitude: 9.06908,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    };
-
-    var MarkerArray = [
-      {
-        ID: 6,
-        GPSLatitude: 5.466131,
-        GPSLongitude: 9.06908,
-        icon: 'https://www2.macs.hw.ac.uk/~sb169/PictoHunt/SamplePhotos/Animals/e4.jpg'
-      },
-      {
-        ID: 5,
-        GPSLatitude: 5.464497,
-        GPSLongitude: 9.071827,
-        icon: 'https://www2.macs.hw.ac.uk/~sb169/PictoHunt/SamplePhotos/Animals/e1.jpg'
-      },
-      {
-        ID: 3,
-        GPSLatitude: 51.510357,
-        GPSLongitude: -0.116773,
-        icon: 'https://www2.macs.hw.ac.uk/~sb169/PictoHunt/SamplePhotos/Architecture/ben3.jpg'
-      },
-      {
-        ID: 2,
-        GPSLatitude: 51.510357,
-        GPSLongitude: -0.116773,
-        icon: 'https://www2.macs.hw.ac.uk/~sb169/PictoHunt/SamplePhotos/Architecture/ben4.jpg'
-      },
-      {
-        ID: 1,
-        GPSLatitude: 51.510357,
-        GPSLongitude: -0.116773,
-        icon: 'https://www2.macs.hw.ac.uk/~sb169/PictoHunt/SamplePhotos/Architecture/ben1.jpg'
+    constructor() {
+      super();
+      this.state={
+        initialRegion: {
+          latitude: 0,
+          longitude:0,
+          latitudeDelta: 4,
+          longitudeDelta: 4,
+        },
+        MarkerArray: [],
       }
-    ]
-    
-    return (
+    }
+
+  componentDidMount() {
+      fetch('http://10.0.2.2:5000/map')
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({ MarkerArray: response })
+      })
+      .catch((error) => {
+       console.log("Error",error);
+     });
+  }
+
+  render(){
+    return(
         <SafeAreaView style={styles.container}>
           <MapView
             style={StyleSheet.absoluteFillObject}
             provider={MapView.PROVIDER_GOOGLE}
-            initialRegion={initialRegion}
+            initialRegion={this.state.initialRegion}
             mapType='standard'
           >
-            {MarkerArray.map( m => {
+            {this.state.MarkerArray.map( m => {
               return(
-                <MapView.Marker coordinate={{latitude: m.GPSLatitude, longitude: m.GPSLongitude}} key={m.ID}>
+                <MapView.Marker coordinate={{latitude: m.GPS._latitude, longitude: m.GPS._longitude}} key={m.ID}>
                   <View style={{flexDirection: 'column', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
                     <Image source={{uri: m.icon}} style={{ width: 80, height: 80 }} />
                     <Text style={{backgroundColor: "#fff"}}> {m.ID} </Text>
@@ -70,10 +58,10 @@ export default function Map() {
                 </MapView.Marker>
               );
             })}
-
           </MapView>
         </SafeAreaView>
-    );
+    )
+  }
 }
 const styles = StyleSheet.create({
     container: {
