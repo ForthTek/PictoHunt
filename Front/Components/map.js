08ed51ec7a78cb8deb9cdd1d71d8e9b510915960
Cameron <55835365+CJB15@users.noreply.github.com>
2021-03-02 21:00:08 +0000

    Map now defaults to users location if possible.

--- Front/Components/map.js
+++ Front/Components/map.js
@@ -1,4 +1,4 @@
-import React, {useState, Component} from "react";
+import React, {useState, useEffect, Component} from "react";
 import { Text, StyleSheet, SafeAreaView, View, Image , Alert } from "react-native";
 import MapView, { Marker } from 'react-native-maps';
 import Geolocation from 'react-native-geolocation-service';
@@ -18,17 +18,37 @@ export default class Map extends Component{
     constructor() {
       super();
       this.state={
-        initialRegion: {
-          latitude: 0,
-          longitude:0,
-          latitudeDelta: 4,
-          longitudeDelta: 4,
-        },
+        latitude: 0,
+        longitude: 0,
+        latitudeDelta: 200,
+        longitudeDelta: 200,
+        locationset: false,
         MarkerArray: [],
       }
     }
 
-  componentDidMount() {
+  async componentDidMount() {
+    console.log("Components mounted.");
+    try {
+      let { status } = await Location.requestPermissionsAsync();
+       if (status !== 'granted') {
+         console.log("Location Premission Refused!");
+         this.setState({ locationset: true })
+         Alert.alert("Location Premission Not Granted!", "To have this screen default to your location please enable location services for this app.")
+         return;
+       }
+       let location = await Location.getLastKnownPositionAsync();
+       this.setState({ latitude: location.coords.latitude })
+       this.setState({ longitude: location.coords.longitude })
+       this.setState({ latitudeDelta: 0.2 })
+       this.setState({ longitudeDelta: 0.2 })
+       this.setState({ locationset: true })
+    } catch (error) {
+       console.log(error);
+       Alert.alert("Error reading location!", "If using an Emulator please ensure a location is selected in the emulator options.")
+       this.setState({ locationset: true })
+    }
+
       fetch('http://10.0.2.2:5000/map')
       .then((response) => response.json())
       .then((response) => {
@@ -40,27 +60,36 @@ export default class Map extends Component{
   }
 
   render(){
-    return(
-        <SafeAreaView style={styles.container}>
-          <MapView
-            style={StyleSheet.absoluteFillObject}
-            provider={MapView.PROVIDER_GOOGLE}
-            initialRegion={this.state.initialRegion}
-            mapType='standard'
-          >
-            {this.state.MarkerArray.map( m => {
-              return(
-                <MapView.Marker coordinate={{latitude: m.GPS._latitude, longitude: m.GPS._longitude}} key={m.ID}>
-                  <View style={{flexDirection: 'column', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-                    <Image source={{uri: m.icon}} style={{ width: 80, height: 80 }} />
-                    <Text style={{backgroundColor: "#fff"}}> {m.ID} </Text>
-                  </View>
-                </MapView.Marker>
-              );
-            })}
-          </MapView>
-        </SafeAreaView>
-    )
+    if(this.state.locationset == true){
+      return(
+          <SafeAreaView style={styles.container}>
+            <MapView
+              style={StyleSheet.absoluteFillObject}
+              provider={MapView.PROVIDER_GOOGLE}
+              initialRegion={{
+                latitude: this.state.latitude,
+                longitude: this.state.longitude,
+                latitudeDelta: this.state.latitudeDelta,
+                longitudeDelta: this.state.longitudeDelta,
+              }}
+              mapType='standard'
+            >
+              {this.state.MarkerArray.map( m => {
+                return(
+                  <MapView.Marker coordinate={{latitude: m.GPS._latitude, longitude: m.GPS._longitude}} key={m.ID}>
+                    <View style={{flexDirection: 'column', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                      <Image source={{uri: m.icon}} style={{ width: 80, height: 80 }} />
+                      <Text style={{backgroundColor: "#fff"}}> {m.ID} </Text>
+                    </View>
+                  </MapView.Marker>
+                );
+              })}
+            </MapView>
+          </SafeAreaView>
+      )
+    } else {
+      return(null)
+    }
   }
 }
 const styles = StyleSheet.create({
