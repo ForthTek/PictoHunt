d6a40a7e0ad5c601057515d48e9a93125b0d9331
ASmithie <alecsmith_@outlook.com>
2021-03-04 16:33:37 +0000

    Broke create account. Will fix

--- Front/Components/Login/signUp.js
+++ Front/Components/Login/signUp.js
@@ -11,105 +11,125 @@ import {
 } from "react-native";
 import * as firebase from "firebase";
 
-export default function Home(props) {
-    var [email, onChangeEmail] = React.useState("");
-    var [username, onChangeName] = React.useState("");
-    var [password, onChangePassword] = React.useState("");
-    var [passwordCon, onChangePasswordCon] = React.useState("");
+export default class SignUp {
+    constructor(props) {
+        this.connection = props.connection;
+    }
 
-    const onSignUp = () => {
+    state = {
+        email: "",
+        username: "",
+        password: "",
+        passwordCon: "",
+    };
+
+    onChangeEmail = (value) => {
+        this.setState({ email: value });
+    };
+
+    onChangeUsername = (value) => {
+        this.setState({ username: value });
+    };
+
+    onChangePassword = (value) => {
+        this.setState({ password: value });
+    };
+
+    onChangePasswordCon = (value) => {
+        this.setState({ passwordCon: value });
+    };
+
+    onSignUp = () => {
         if (password !== passwordCon) {
             Alert.alert("Passwords do not match");
             return;
         }
-        firebase
-            .auth()
-            .createUserWithEmailAndPassword(email, password)
-            .then(
-                () => {},
-                (error) => {
-                    Alert.alert(error.message);
-                }
-            );
+        return;
     };
-
-    return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.container0}>
-                <Image
-                    source={require("../../assets/ForthTek.png")}
-                    width={160}
-                    style={{ width: 160, height: 160 }}
-                />
-                <Text style={{ fontSize: 35 }}>Pictohunt</Text>
-                <Text style={{ fontSize: 18 }}>By ForthTek</Text>
-            </View>
-            <View style={styles.container1}>
-                <View style={styles.loginCon}>
-                    <Text style={styles.text}>Email:</Text>
-
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={(text) => onChangeEmail(text)}
-                        value={email}
-                        placeholder='Email'
-                        autoCapitalize='none'
-                        autoCorrect={false}
-                        keyboardType='email-address'
+    render() {
+        return (
+            <SafeAreaView style={styles.container}>
+                <View style={styles.container0}>
+                    <Image
+                        source={require("../../assets/ForthTek.png")}
+                        width={160}
+                        style={{ width: 160, height: 160 }}
                     />
+                    <Text style={{ fontSize: 35 }}>Pictohunt</Text>
+                    <Text style={{ fontSize: 18 }}>By ForthTek</Text>
+                </View>
+                <View style={styles.container1}>
+                    <View style={styles.loginCon}>
+                        <Text style={styles.text}>Email:</Text>
+
+                        <TextInput
+                            style={styles.input}
+                            onChangeText={(text) => this.onChangeEmail(text)}
+                            value={this.state.email}
+                            placeholder='Email'
+                            autoCapitalize='none'
+                            autoCorrect={false}
+                            keyboardType='email-address'
+                        />
+                    </View>
+
+                    <View style={styles.loginCon}>
+                        <Text style={styles.text}>Username:</Text>
+
+                        <TextInput
+                            style={styles.input}
+                            onChangeText={(text) => this.onChangeName(text)}
+                            value={this.state.username}
+                            placeholder='UserName'
+                            autoCapitalize='none'
+                            autoCorrect={false}
+                        />
+                    </View>
+
+                    <View style={styles.loginCon}>
+                        <Text style={styles.text}>Password:</Text>
+
+                        <TextInput
+                            style={styles.input}
+                            onChangeText={(text) => this.onChangePassword(text)}
+                            value={this.state.password}
+                            placeholder='Password'
+                            secureTextEntry={true}
+                            autoCapitalize='none'
+                            autoCorrect={false}
+                        />
+                    </View>
+
+                    <View style={styles.loginCon}>
+                        <Text style={styles.text}>Password:</Text>
+
+                        <TextInput
+                            style={styles.input}
+                            onChangeText={(text) =>
+                                this.onChangePasswordCon(text)
+                            }
+                            value={this.state.passwordCon}
+                            placeholder='Re-type Password'
+                            secureTextEntry={true}
+                            autoCapitalize='none'
+                            autoCorrect={false}
+                        />
+                    </View>
+
+                    <View style={styles.button}>
+                        <Button
+                            onPress={this.onSignUp()}
+                            title='Sign Up'
+                        ></Button>
+                    </View>
                 </View>
 
-                <View style={styles.loginCon}>
-                    <Text style={styles.text}>Username:</Text>
-
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={(text) => onChangeName(text)}
-                        value={username}
-                        placeholder='UserName'
-                        autoCapitalize='none'
-                        autoCorrect={false}
-                    />
+                <View style={styles.button0}>
+                    <Button title='back' onPress={this.props.back} />
                 </View>
-
-                <View style={styles.loginCon}>
-                    <Text style={styles.text}>Password:</Text>
-
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={(text) => onChangePassword(text)}
-                        value={password}
-                        placeholder='Password'
-                        secureTextEntry={true}
-                        autoCapitalize='none'
-                        autoCorrect={false}
-                    />
-                </View>
-
-                <View style={styles.loginCon}>
-                    <Text style={styles.text}>Password:</Text>
-
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={(text) => onChangePasswordCon(text)}
-                        value={passwordCon}
-                        placeholder='Re-type Password'
-                        secureTextEntry={true}
-                        autoCapitalize='none'
-                        autoCorrect={false}
-                    />
-                </View>
-
-                <View style={styles.button}>
-                    <Button onPress={onSignUp} title='Sign Up'></Button>
-                </View>
-            </View>
-
-            <View style={styles.button0}>
-                <Button title='back' onPress={props.back} />
-            </View>
-        </SafeAreaView>
-    );
+            </SafeAreaView>
+        );
+    }
 }
 const styles = StyleSheet.create({
     container: {
