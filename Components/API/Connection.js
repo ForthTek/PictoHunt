6bc9a77f3de923d61923d3279c449febcc52b995
ASmithie <alecsmith_@outlook.com>
2021-03-23 16:34:25 +0000

    Big time changes searchign users and stuff is outstanding

--- Components/API/Connection.js
+++ Components/API/Connection.js
@@ -3,300 +3,309 @@ import Firebase from "./Firebase.js";
 import Server from "./Server.js";
 
 export default class Connection {
-  #firebase;
-  #server;
+    #firebase;
+    #server;
 
-  constructor() {
-    this.#firebase = new Firebase();
-    this.#server = new Server();
-  }
-
-  /**
-   * Invokes the function with no parameters, whenever the login state of the user changes
-   *
-   * @param {function} callback
-   */
-  addLoginStateListener = (callback) => {
-    return this.#firebase.addLoginStateListener(callback);
-  };
-
-  /**
-   * @returns true if there is a user logged in
-   */
-  isLoggedIn = () => {
-    return this.#firebase.isLoggedIn();
-  };
-
-  /**
-   * @returns JSON with .username and .email
-   */
-  currentUser = () => {
-    return this.#firebase.currentUser();
-  };
-
-  /**
-   *
-   * @param {string} email
-   * @param {string} password
-   * @returns true if successful
-   */
-  login = async (email, password) => {
-    return await this.#firebase.login(email, password);
-  };
-
-  /**
-   * @returns true if succesful
-   */
-  logout = async () => {
-    return await this.#firebase.logout();
-  };
-
-  /**
-   *
-   * @param {string} email
-   * @returns true if succesful
-   */
-  resetPassword = async (email) => {
-    return await this.#firebase.resetPassword(email);
-  };
-
-  /**
-   *
-   * @param {string} email
-   * @param {string} username
-   * @param {string} password
-   * @param {boolean} isPublic
-   * @returns true if succesful
-   */
-  createProfile = async (email, username, password, isPublic = true) => {
-    if (await this.#server.containsSwears(username)) {
-      throw new Error(`Username ${username} contains a swear`);
+    constructor() {
+        this.#firebase = new Firebase();
+        this.#server = new Server();
     }
 
-    return await this.#firebase.createProfile(
-      email,
-      username,
-      password,
-      isPublic
-    );
-  };
+    /**
+     * Invokes the function with no parameters, whenever the login state of the user changes
+     *
+     * @param {function} callback
+     */
+    addLoginStateListener = (callback) => {
+        return this.#firebase.addLoginStateListener(callback);
+    };
 
-  /**
-   *
-   * @param {string} postID
-   * @returns
-   */
-  likePost = async (postID) => {
-    return await this.#firebase
-      .interactWithPost(postID, this.#firebase.PostInteractionType.like)
-      .then(
-        async () => {
-          return await this.#server.updatePostValues(postID);
-        },
-        (error) => {
-          console.log(error);
-          throw new Error(`Server failed to update post ${postID}`);
+    /**
+     * @returns true if there is a user logged in
+     */
+    isLoggedIn = () => {
+        return this.#firebase.isLoggedIn();
+    };
+
+    /**
+     * @returns JSON with .username and .email
+     */
+    currentUser = () => {
+        return this.#firebase.currentUser();
+    };
+
+    /**
+     *
+     * @param {string} email
+     * @param {string} password
+     * @returns true if successful
+     */
+    login = async (email, password) => {
+        return await this.#firebase.login(email, password);
+    };
+
+    /**
+     * @returns true if succesful
+     */
+    logout = async () => {
+        return await this.#firebase.logout();
+    };
+
+    /**
+     *
+     * @param {string} email
+     * @returns true if succesful
+     */
+    resetPassword = async (email) => {
+        return await this.#firebase.resetPassword(email);
+    };
+
+    /**
+     *
+     * @param {string} email
+     * @param {string} username
+     * @param {string} password
+     * @param {boolean} isPublic
+     * @returns true if succesful
+     */
+    createProfile = async (email, username, password, isPublic = true) => {
+        if (await this.#server.containsSwears(username)) {
+            throw new Error(`Username ${username} contains a swear`);
         }
-      )
-      .then(async (res) => {
-        return this.#firebase.getPost(postID);
-      });
-  };
 
-  /**
-   *
-   * @param {string} postID
-   * @returns
-   */
-  dislikePost = async (postID) => {
-    return await this.#firebase
-      .interactWithPost(postID, this.#firebase.PostInteractionType.dislike)
-      .then(
-        async () => {
-          return await this.#server.updatePostValues(postID);
-        },
-        (error) => {
-          console.log(error);
-          throw new Error(`Server failed to update post ${postID}`);
+        return await this.#firebase.createProfile(
+            email,
+            username,
+            password,
+            isPublic
+        );
+    };
+
+    /**
+     *
+     * @param {string} postID
+     * @returns
+     */
+    likePost = async (postID) => {
+        return await this.#firebase
+            .interactWithPost(postID, this.#firebase.PostInteractionType.like)
+            .then(
+                async () => {
+                    return await this.#server.updatePostValues(postID);
+                },
+                (error) => {
+                    console.log(error);
+                    throw new Error(`Server failed to update post ${postID}`);
+                }
+            )
+            .then(async (res) => {
+                return this.#firebase.getPost(postID);
+            });
+    };
+
+    /**
+     *
+     * @param {string} postID
+     * @returns
+     */
+    dislikePost = async (postID) => {
+        return await this.#firebase
+            .interactWithPost(
+                postID,
+                this.#firebase.PostInteractionType.dislike
+            )
+            .then(
+                async () => {
+                    return await this.#server.updatePostValues(postID);
+                },
+                (error) => {
+                    console.log(error);
+                    throw new Error(`Server failed to update post ${postID}`);
+                }
+            )
+            .then(async (res) => {
+                return this.#firebase.getPost(postID);
+            });
+    };
+
+    /**
+     *
+     * @param {string} postID
+     * @returns
+     */
+    removeInteractionFromPost = async (postID) => {
+        return await this.#firebase
+            .interactWithPost(postID, this.#firebase.PostInteractionType.remove)
+            .then(
+                async () => {
+                    return await this.#server.updatePostValues(postID);
+                },
+                (error) => {
+                    console.log(error);
+                    throw new Error(`Server failed to update post ${postID}`);
+                }
+            )
+            .then(async (res) => {
+                return this.#firebase.getPost(postID);
+            });
+    };
+
+    /**
+     * Loads the post from local memory if it has been loaded before, or gets it from the server if not.
+     *
+     * @param {string} postID
+     * @returns
+     */
+    getPost = async (postID) => {
+        return await this.#firebase.getPost(postID);
+    };
+
+    /**
+     *
+     * @param {Filter} filter
+     * @returns
+     */
+    getBrowse = async (filter = new Filter()) => {
+        return await this.#firebase.getBrowse(filter);
+    };
+
+    /**
+     *
+     * @param {Filter} filter
+     * @returns
+     */
+    getAllPosts = async (filter = new Filter()) => {
+        return await this.#firebase.getAllPosts(filter);
+    };
+
+    /**
+     *
+     * @returns
+     */
+    getMap = async () => {
+        return await this.#firebase.getMap();
+    };
+
+    /**
+     *
+     * @returns
+     */
+    getOurProfile = async () => {
+        const user = this.#firebase.currentUser();
+        return await this.getProfile(user.username);
+    };
+
+    /**
+     *
+     * @param {string} username
+     * @param {boolean} loadFollowedFeeds
+     */
+    getProfile = async (username, filter = new Filter()) => {
+        return await this.#firebase.getProfile(username, filter);
+    };
+
+    /**
+     *
+     * @param {string} title
+     * @param {string} channelName
+     * @param {number} latitude
+     * @param {number} longitude
+     * @param {Blob[]} photos
+     * @returns
+     */
+    createPost = async (title, channelName, latitude, longitude, photos) => {
+        // Filter any swears from the title
+        const newTitle = await this.#server.filterSwears(title);
+
+        return await this.#firebase
+            // Create the post
+            .createPost(newTitle, channelName, latitude, longitude, photos)
+            .then(async (newKey) => {
+                // Get the server to approve the post
+                await this.#server.approvePost(newKey);
+                // Then update the number of likes etc
+                //await this.#server.updatePostValues(newKey);
+                return newKey;
+            });
+    };
+
+    /**
+     *
+     * @param {string} name
+     * @param {string} description
+     * @returns
+     */
+    createChannel = async (name, description) => {
+        // Ensure that the channel name and description is clean
+        if (await this.#server.containsSwears(name)) {
+            throw new Error(`Channel name ${name} contains a swear`);
         }
-      )
-      .then(async (res) => {
-        return this.#firebase.getPost(postID);
-      });
-  };
-
-  /**
-   *
-   * @param {string} postID
-   * @returns
-   */
-  removeInteractionFromPost = async (postID) => {
-    return await this.#firebase
-      .interactWithPost(postID, this.#firebase.PostInteractionType.remove)
-      .then(
-        async () => {
-          return await this.#server.updatePostValues(postID);
-        },
-        (error) => {
-          console.log(error);
-          throw new Error(`Server failed to update post ${postID}`);
+        if (await this.#server.containsSwears(description)) {
+            throw new Error(
+                `Channel description ${description} contains a swear`
+            );
         }
-      )
-      .then(async (res) => {
-        return this.#firebase.getPost(postID);
-      });
-  };
 
-  /**
-   * Loads the post from local memory if it has been loaded before, or gets it from the server if not.
-   *
-   * @param {string} postID
-   * @returns
-   */
-  getPost = async (postID) => {
-    return await this.#firebase.getPost(postID);
-  };
+        return await this.#firebase.createChannel(name, description);
+    };
 
-  /**
-   *
-   * @param {Filter} filter
-   * @returns
-   */
-  getBrowse = async (filter = new Filter()) => {
-    return await this.#firebase.getBrowse(filter);
-  };
+    /**
+     *
+     * @param {string} name
+     * @returns
+     */
+    getChannel = async (name, filter = new Filter()) => {
+        return await this.#firebase.getChannel(name, filter);
+    };
 
-  /**
-   *
-   * @param {Filter} filter
-   * @returns
-   */
-  getAllPosts = async (filter = new Filter()) => {
-    return await this.#firebase.getAllPosts(filter);
-  };
+    /**
+     *
+     * @param {string} usernameToFollow
+     * @param {boolean} value
+     */
+    followUser = async (usernameToFollow, value) => {
+        return await this.#firebase.followUser(usernameToFollow, value);
+    };
 
-  /**
-   *
-   * @returns
-   */
-  getMap = async () => {
-    return await this.#firebase.getMap();
-  };
+    /**
+     *
+     * @param {string} channelNameToFollow
+     * @param {boolean} value
+     */
+    followChannel = async (channelNameToFollow, value) => {
+        return await this.#firebase.followChannel(channelNameToFollow, value);
+    };
 
-  /**
-   *
-   * @returns
-   */
-  getOurProfile = async () => {
-    const user = this.#firebase.currentUser();
-    return await this.getProfile(user.username);
-  };
+    /**
+     *
+     * @param {string} text
+     * @returns
+     */
+    searchChannels = async (text) => {
+        return await this.#firebase.searchWithPrefix("Channels", text);
+    };
 
-  /**
-   *
-   * @param {string} username
-   * @param {boolean} loadFollowedFeeds
-   */
-  getProfile = async (username, filter = new FIlter()) => {
-    return await this.#firebase.getProfile(username, filter);
-  };
+    /**
+     *
+     * @param {string} text
+     * @returns
+     */
+    searchUsers = async (text) => {
+        return await this.#firebase.searchWithPrefix("Users", text);
+    };
 
-  /**
-   *
-   * @param {string} title
-   * @param {string} channelName
-   * @param {number} latitude
-   * @param {number} longitude
-   * @param {Blob[]} photos
-   * @returns
-   */
-  createPost = async (title, channelName, latitude, longitude, photos) => {
-    // Filter any swears from the title
-    const newTitle = await this.#server.filterSwears(title);
-
-    return await this.#firebase
-      // Create the post
-      .createPost(newTitle, channelName, latitude, longitude, photos)
-      .then(async (newKey) => {
-        // Get the server to approve the post
-        await this.#server.approvePost(newKey);
-        // Then update the number of likes etc
-        //await this.#server.updatePostValues(newKey);
-        return newKey;
-      });
-  };
-
-  /**
-   *
-   * @param {string} name
-   * @param {string} description
-   * @returns
-   */
-  createChannel = async (name, description) => {
-    // Ensure that the channel name and description is clean
-    if (await this.#server.containsSwears(name)) {
-      throw new Error(`Channel name ${name} contains a swear`);
-    }
-    if (await this.#server.containsSwears(description)) {
-      throw new Error(`Channel description ${description} contains a swear`);
-    }
-
-    return await this.#firebase.createChannel(name, description);
-  };
-
-  /**
-   *
-   * @param {string} name
-   * @returns
-   */
-  getChannel = async (name, filter = new Filter()) => {
-    return await this.#firebase.getChannel(name, filter);
-  };
-
-  /**
-   *
-   * @param {string} usernameToFollow
-   * @param {boolean} value
-   */
-  followUser = async (usernameToFollow, value) => {
-    return await this.#firebase.followUser(usernameToFollow, value);
-  };
-
-  /**
-   *
-   * @param {string} channelNameToFollow
-   * @param {boolean} value
-   */
-  followChannel = async (channelNameToFollow, value) => {
-    return await this.#firebase.followChannel(channelNameToFollow, value);
-  };
-
-  /**
-   *
-   * @param {string} text
-   * @returns
-   */
-  searchChannels = async (text) => {
-    return await this.#firebase.searchWithPrefix("Channels", text);
-  };
-
-  /**
-   *
-   * @param {string} text
-   * @returns
-   */
-  searchUsers = async (text) => {
-    return await this.#firebase.searchWithPrefix("Users", text);
-  };
-
-  /**
-   *
-   * @param {Date} deadline
-   * @param {number} score
-   * @param {object[]} tasksPerPost Array of JSON objects containing .channel (channel name), .latitude and .longitude (required location)
-   * @returns
-   */
-  createChallenge = async (deadline, score, tasksPerPost) => {
-    return await this.#firebase.createChallenge(deadline, score, tasksPerPost);
-  };
+    /**
+     *
+     * @param {Date} deadline
+     * @param {number} score
+     * @param {object[]} tasksPerPost Array of JSON objects containing .channel (channel name), .latitude and .longitude (required location)
+     * @returns
+     */
+    createChallenge = async (deadline, score, tasksPerPost) => {
+        return await this.#firebase.createChallenge(
+            deadline,
+            score,
+            tasksPerPost
+        );
+    };
 }
--- Components/userModal.js
+++ Components/userModal.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import Score from "./score";
-
-import { Text, StyleSheet, View } from "react-native";
+import Post from "./post";
+import SinglePost from "./singlePost";
+import { Text, StyleSheet, View, FlatList } from "react-native";
 export default class UserModal extends Component {
     constructor(props) {
         super(props);
@@ -10,8 +11,10 @@ export default class UserModal extends Component {
 
     state = {
         DATA: this.props.DATA,
-        postDATA: "",
-        posts: false,
+        postDATA: this.props.DATA.posts,
+        hasPosts: true,
+        singlePost: false,
+        singlePostID: "",
     };
 
     // componentDidMount() {
@@ -22,9 +25,64 @@ export default class UserModal extends Component {
     //         });
     // }
 
+    getID = (id) => {
+        let i = 0;
+        for (; i < this.state.postDATA.length; i++) {
+            if (id == this.state.postDATA[i].ID) {
+                this.setState({ singlePostID: i });
+            }
+        }
+        this.handleSinglePost();
+    };
+
+    handleSinglePost = () => {
+        this.setState({ isPost: !this.state.isPost });
+    };
+
+    onLikeBtnPress = (type, id, updateScore) => {
+        if (type === "like") {
+            alert("Pressed: " + type);
+            this.connection.likePost(id).then(
+                () => {
+                    updateScore(id);
+                },
+                (error) => {
+                    Alert.alert(error.message);
+                }
+            );
+        }
+        if (type === "dislike") {
+            alert("Pressed: " + type);
+            this.connection
+                .dislikePost(id)
+                .then(
+                    () => {
+                        updateScore(id);
+                    },
+                    (error) => {
+                        Alert.alert(error.message);
+                    }
+                )
+                .catch((error) => {
+                    Alert.alert(error.message);
+                });
+        }
+        if (type === "remove") {
+            alert("Pressed: " + type);
+            this.connection.removeInteractionFromPost(id).then(
+                () => {
+                    updateScore(id);
+                },
+                (error) => {
+                    Alert.alert(error.message);
+                }
+            );
+        }
+    };
+
     render() {
         return (
-            <View>
+            <View style={styles.bigCon}>
                 <Text style={styles.text}>{this.state.DATA.username}</Text>
                 <View
                     style={{
@@ -34,11 +92,12 @@ export default class UserModal extends Component {
                     }}
                 >
                     <Score label='Score' number={this.state.DATA.score} />
-                    <Score label='Pics' number={this.state.DATA.totalPosts} />
+                    <Score label='Pics' number={this.state.postDATA.length} />
                     <Score label='Rank' number={5} />
                 </View>
-                {this.state.posts && (
+                {this.state.hasPosts && !this.state.singlePost && (
                     <FlatList
+                        style={styles.list}
                         data={this.state.postDATA}
                         renderItem={({ item }) => (
                             <View style={styles.post}>
@@ -53,6 +112,16 @@ export default class UserModal extends Component {
                         keyExtractor={(item) => item.ID.toString()}
                     />
                 )}
+                {this.state.hasPosts && this.state.singlePost && (
+                    <View style={styles.container}>
+                        <SinglePost
+                            item={this.state.postDATA[this.state.singlePostID]}
+                            back={this.handleSinglePost}
+                            connection={this.connection}
+                            onLikeBtnPress={this.onLikeBtnPress}
+                        />
+                    </View>
+                )}
             </View>
         );
     }
@@ -64,8 +133,20 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "center",
     },
+    bigCon: {
+        height: "90%",
+    },
     text: {
         fontSize: 32,
         paddingLeft: "2%",
+    },
+    post: {
+        borderColor: "grey",
+        borderTopWidth: 1,
+        borderBottomWidth: 1,
+    },
+    list: {
+        flexGrow: 0,
+        height: "90%",
     },
 });
