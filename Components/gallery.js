13b6d36ec409319326626a4252f0dad1839dcd0d
Cameron <55835365+CJB15@users.noreply.github.com>
2021-03-24 23:30:22 +0000

    Deleted unnecessary file

--- Components/gallery.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import Score from "./score";
-import Post from "./post";
-import SinglePost from "./singlePost";
-import { Text, StyleSheet, View, FlatList, Pressable, Alert } from "react-native";
-import FeatherIcon from "react-native-vector-icons/Feather";
-
-export default class UserModal extends Component {}
