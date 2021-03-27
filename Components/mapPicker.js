107bbc904b81fd66e6420748aa056795c7a5e0ac
Cameron <55835365+CJB15@users.noreply.github.com>
2021-03-27 16:51:41 +0000

    Fixed issue with API key Implementation

--- Components/mapPicker.js
+++ Components/mapPicker.js
@@ -10,6 +10,7 @@ import {
 } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import * as Location from "expo-location";
+import { PROVIDER_GOOGLE } from "react-native-maps";
 
 // NOTE: internal commentary dosn't have spell check, sorry
 
@@ -141,7 +142,7 @@ export default class Map extends Component {
                 <SafeAreaView style={styles.container}>
                     <MapView
                         style={StyleSheet.absoluteFillObject}
-                        provider={MapView.PROVIDER_GOOGLE}
+                        provider={PROVIDER_GOOGLE}
                         initialRegion={{
                             latitude: this.state.latitude,
                             longitude: this.state.longitude,
